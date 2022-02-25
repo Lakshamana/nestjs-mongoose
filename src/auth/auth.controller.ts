e9e6e354506e9a6689e891b7ab73008854336c2e
Lakshamana <lakshamana@protonmail.com>
2022-02-24 23:40:33 -0300

    chore: set public guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common'
+import { SetPublic } from '@root/common/decorators/set-public.decorator'
 import { AuthService } from './auth.service'
 import { LocalAuthGuard } from './local-auth.guard'
 
@@ -7,6 +8,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @SetPublic()
   @Post('/login')
   async login(@Request() req) {
     return this.authService.login(req.user)
